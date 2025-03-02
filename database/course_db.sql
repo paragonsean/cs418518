d3fb7c1e751f9c1b93764d106277ef3b415e4ef5
Sean Baker <sbake021@odu.edu>
2025-03-01 20:59:11 -0500

    fixed db

--- database/course_db.sql
+++ database/course_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 17, 2025 at 05:04 AM
+-- Generation Time: Mar 02, 2025 at 02:58 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -28,26 +28,28 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `user` (
-  `u_id` int(255) NOT NULL,
+  `u_id` int(11) NOT NULL,
   `u_first_name` varchar(20) NOT NULL,
   `u_last_name` varchar(20) NOT NULL,
   `u_email` varchar(35) NOT NULL,
   `u_password` varchar(200) NOT NULL,
-  `is_approved` tinyint(1) NOT NULL,
-  `is_admin` tinyint(1) NOT NULL
+  `is_approved` tinyint(1) NOT NULL DEFAULT 0,
+  `is_admin` tinyint(1) NOT NULL DEFAULT 0,
+  `verification_code` varchar(6) DEFAULT NULL,
+  `otp_expires_at` datetime DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `verification_token` text DEFAULT NULL,
+  `is_verified` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`u_id`, `u_first_name`, `u_last_name`, `u_email`, `u_password`, `is_approved`, `is_admin`) VALUES
-(1, 'Admin', '', '', '$2b$10$xLMFe8hBE4vxQ6r/kzorxefS0MhL3g1BO7dYsO/pb.QQSJhE3k6n6', 1, 1),
-(4, 'test', 'test', 'test@test.com', '$2b$10$fihH187/r4aU/HpD6YwFUOaw6rCjrhSyssQiE0IIqIf75S9xiTRzW', 1, 0),
-(19, 'james', 'baxter', 'test@gmail.com', '$2b$10$o78ryBTXGbRKy9lqe2TwveB8ndGGtOqI6ep.mGv1XSwzi95aaS/y6', 1, 0),
-(64, 'brad', 'wey', 'bradway@email.com', '$2b$10$xLMFe8hBE4vxQ6r/kzorxefS0MhL3g1BO7dYsO/pb.QQSJhE3k6n6', 1, 0),
-(79, 'SEAn', 'B', 'sbake021@odu.edu', '$2b$10$xLMFe8hBE4vxQ6r/kzorxefS0MhL3g1BO7dYsO/pb.QQSJhE3k6n6', 1, 0),
-(81, 'sean', 'baker', 'seancameronbaker@gmail.com', '$2b$10$xLMFe8hBE4vxQ6r/kzorxefS0MhL3g1BO7dYsO/pb.QQSJhE3k6n6', 1, 1);
+INSERT INTO `user` (`u_id`, `u_first_name`, `u_last_name`, `u_email`, `u_password`, `is_approved`, `is_admin`, `verification_code`, `otp_expires_at`, `created_at`, `updated_at`, `verification_token`, `is_verified`) VALUES
+(1, 'Unknown', 'User', 'seancameronbaker@gmail.com', '$2b$10$0dp71J3Y.a1FFczgwHA0HO6Z1HmaRRyLG1Oxq/qTTCeev1OyjzO9K', 0, 0, NULL, NULL, '2025-03-01 06:24:45', '2025-03-02 00:53:25', NULL, 1),
+(9, 'sean', 'baker', 'cos30degrees@gmail.com', '$2b$10$k7dOQLWyNYjJZzmnkG58HO6P7yV4M1LhNiHXMmjCb6kZX3JRjjQ7.', 0, 0, NULL, NULL, '2025-03-02 00:36:47', '2025-03-02 01:32:23', NULL, 1);
 
 --
 -- Indexes for dumped tables
@@ -68,7 +70,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `u_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
+  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
