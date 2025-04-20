bfb3373f0ccdad735ac5e76f93c789aad8715bbc
Sean Baker <cos30degrees@gmail.com>
2025-04-19 20:25:41 -0400

    Milestone3s

--- server/dist/services/password_validator.js
+++
@@ -1,4 +0,0 @@
-export const isStrongPassword = password => {
-  const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#^+=(){}\[\]])[A-Za-z\d@$!%*?&#^+=(){}\[\]]{8,}$/;
-  return PASSWORD_REGEX.test(password);
-};
